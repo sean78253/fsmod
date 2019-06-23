830df5e44a409ecc3330bc5f3ab12788c493e4e0
sean78253 <feb09@usersunion.net>
2019-06-22 21:16:13 -0500

    Fix up Auto increments

--- fsmob-setup.py
+++ fsmob-setup.py
@@ -35,12 +35,23 @@ balanace DECIMAL(6,2),
 account_status VARCHAR(10) NOT NULL DEFAULT 'ACTIVE') ENGINE=INNODB
 """)
 
+cursor.execute("""CREATE TABLE ClusterMonitor(
+Customer_id INT NOT NULL,
+INDEX Cluster_Customer_ind (Customer_id),
+FOREIGN KEY (Customer_id) REFERENCES Customer(Customer_id) ON DELETE CASCADE,
+Cluster varchar(17) NOT NULL,
+common_name varchar(20) NOT NULL,
+date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+last_seen TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+INDEX Cluster_Seen_ind (Customer_id, Cluster, date_added)) ENGINE=INNODB
+""")
+
 cursor.execute("""CREATE TABLE Sensor(
 Customer_id INT NOT NULL,
 INDEX CustomerSensor_ind (Customer_id), 
 FOREIGN KEY (Customer_id) REFERENCES Customer(Customer_id) ON DELETE CASCADE, 
 datesample TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-cm varchar(20) NOT NULL,
+cm varchar(17) NOT NULL,
 sensorserial varchar(50) NOT NULL,
 sensortype varchar(2) NOT NULL,
 sensordata DECIMAL(5,5) NOT NULL) ENGINE=INNODB
@@ -50,12 +61,15 @@ cursor.execute("""CREATE TABLE Notes(
 Customer_id INT NOT NULL,
 INDEX CustomerNotes_ind (Customer_id),
 FOREIGN KEY (Customer_id) REFERENCES Customer(Customer_id) ON DELETE CASCADE,
-Person_id INT not null,
+Person_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 priority varchar(10),
 Note Tinyblob,
 INDEX Notes_idx (Customer_id, Person_id)) ENGINE=INNODB
 """)
+
+cursor.execute(""" ALTER TABLE Customer AUTO_INCREMENT=100 """)
+cursor.execute(""" ALTER TABLE Notes AUTO_INCREMENT=100 """)
 
 cursor.close()
 
